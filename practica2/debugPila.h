13a5cabbf0788ebe855b0cf69785a828196fcf57
Alex Oarga <alex718123@gmail.com>
2017-11-06 22:46:56 +0100

    Add files via upload

---
+++ practica2/debugPila.h
@@ -0,0 +1,15 @@
+/*********************************************************************************************
+* Fichero:		timer.h
+* Autor:
+* Descrip:		funciones de control del timer0 del s3c44b0x
+* Version:
+*********************************************************************************************/
+
+#ifndef _EXC_H_
+#define _EXC_H_
+
+void debug_init();
+void push_debug(int ID_evento, int auxData);
+
+
+#endif /* _TIMER_H_ */
